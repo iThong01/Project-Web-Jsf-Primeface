c24a86232fa0e642651075f30b42db3bf09a3614
amZix01 <flagwork63@gmail.com>
2026-04-16 23:45:25 +0700

    change name

---
+++ src/main/java/util/DBUtil.java
@@ -0,0 +1,10 @@
+package util;
+import java.sql.*;
+public class DBUtil {
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/greenmarket?useSSL=false",
+            "root","T12345678");
+    }
+}
